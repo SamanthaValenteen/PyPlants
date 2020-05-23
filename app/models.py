f5e74803394fd4d049f9fcf6053b2239a0697fae
SamanthaValenteen <32467902+SamanthaValenteen@users.noreply.github.com>
2020-05-23 14:28:00 -0700

    Committing first draft. Stores plants for each user and displays when to water next. Next steps: add form to mark as watered and to edit/remove existing plants.

--- app/models.py
+++ app/models.py
@@ -1,11 +1,47 @@
+from datetime import datetime, timedelta
 from app import db
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    plants = db.relationship('Plant', backref='owner', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+
+class Plant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(140))
+    pot = db.Column(db.String(140))
+    watering_frequency = db.Column(db.Integer) #in days
+    last_watered = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    next_water_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def update_water_date(self): #separate function from water() for future scenarios, such as watering frequency being changed
+        self.next_water_date = self.last_watered + timedelta(days=self.watering_frequency)
+
+    def water(self):
+        self.last_watered = datetime.utcnow()
+
+    def __repr__(self):
+        return '<Plant {}>'.format(self.type)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,71 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, PlantForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User, Plant
+from werkzeug.urls import url_parse
+from datetime import timedelta, datetime
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@login_required
 def index():
-    user = {'username': 'Samantha'}
-    posts = [
-        {
-            'author': {'username': "Eric"},
-            'body': 'Boy I really hate sniffs!'
-        },
-        {
-            'author': {'username': "Eric Again"},
-            'body': 'One layer of cake is already too many.'
-        }
+    form = PlantForm()
+    if form.validate_on_submit():
+        new_plant = Plant(type=form.type.data, pot=form.pot.data, watering_frequency=form.watering_frequency.data, user_id=current_user.id, next_water_date=datetime.utcnow() + timedelta(days=form.watering_frequency.data))
+        db.session.add(new_plant)
+        db.session.commit()
+        flash('New plant added!')
+        return redirect(url_for('index'))
 
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    plants = Plant.query.filter_by(user_id=current_user.id)
+    plant_list = []
+    # This loop to update my existing table that didn't have water date populated.
+    for plant in plants:
+        plant.update_water_date()
+        plant_list.append(plant.type)
+    print(plant_list)
+    plant_count = Plant.query.filter_by(user_id=current_user.id).count()
+
+    #TODO add form to update watering date or other properties.
+    #update_form = UpdatePlantForm()
+
+    return render_template('index.html', title='Home', plants=plants, form=form, plant_count=plant_count)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title="sign In", form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful.')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
